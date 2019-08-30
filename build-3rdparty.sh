21a22e3bf350aeba99cf664b1def3619ec706b38
yixinglu <2520865+yixinglu@users.noreply.github.com>
2019-08-30 16:07:46 +0800

    Move git clone outside

--- build-3rdparty.sh
+++ build-3rdparty.sh
@@ -2,12 +2,13 @@
 
 set -x
 
-git lfs install
-git clone https://github.com/vesoft-inc/nebula-3rdparty.git
-cd nebula-3rdparty
-source ~/.bashrc
-cmake .
+export LIBRARY_PATH="/usr/lib/x86_64-linux-gnu:$LIBRARY_PATH"
+
+NEBULA_HOME=/home/nebula
+
+cd $NEBULA_HOME/nebula-3rdparty
+/home/engshare/cmake/bin/cmake -DCMAKE_C_COMPILER=/home/engshare/gcc/bin/gcc -DCMAKE_CXX_COMPILER=/home/engshare/gcc/bin/g++ -DNEBULA_GPERF_BIN_DIR=/home/engshare/gperf/bin -DNEBULA_FLEX_ROOT=/home/engshare/flex -DNEBULA_BOOST_ROOT=/home/engshare/boost -DNEBULA_OPENSSL_ROOT=/home/engshare/openssl -DNEBULA_KRB5_ROOT=/home/engshare/krb5 -DNEBULA_LIBUNWIND_ROOT=/home/engshare/libunwind -DNEBULA_READLINE_ROOT=/home/engshare/readline -DNEBULA_NCURSES_ROOT=/home/engshare/ncurses .
 make
 make install
-cd ..
-rm -rf nebula-3rdparty
+cd $NEBULA_HOME
+rm -rf $NEBULA_HOME/nebula-3rdparty
