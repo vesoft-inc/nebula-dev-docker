7578c3e90df11ac99615fa5e7833115299219af8
yixinglu <2520865+yixinglu@users.noreply.github.com>
2019-09-05 00:14:55 +0800

    Accept tag argument from outside

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+TAG=${2:-latest}
 
 docker run --rm -ti \
   --security-opt seccomp=unconfined \
   -v "$1":/home/nebula \
   --name nebula \
-  nebula-dev:latest \
+  nebula-dev:$TAG \
   bash
