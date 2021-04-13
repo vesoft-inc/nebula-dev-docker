2ad87878ce4d5342b22313f51e2857ddd9856377
yixinglu <2520865+yixinglu@users.noreply.github.com>
2021-04-13 16:03:57 +0800

    Restore run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+TAG=${2:-latest}
+
+docker run --rm -ti \
+  --security-opt seccomp=unconfined \
+  -v "$1":/home/nebula \
+  -w /home/nebula \
+  --name nebula_$USER \
+  vesoft/nebula-dev:$TAG \
+  bash
